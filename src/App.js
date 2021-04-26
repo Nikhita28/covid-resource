f524d72af7bf1ad3aab1a6efc8f6020b2363419e
Nikhita <vnikhita28@gmail.com>
2021-04-26 14:54:41 +0530

    added repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Location from './Location'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="heading">Together, we can fight Covid-19</h1>
+      <h2 className="sub-heading">Which resource are you looking for?</h2>
+      <div className="buttons">
+        <a className="btn btn-outline-light" href="#medicine">Medicines</a>
+        <a className="btn btn-outline-light" href="#plasma">Plasma</a>
+        <a className="btn btn-outline-light" href="#oxygen">Oxygen</a>
+        <a className="btn btn-outline-light" href="#beds">Beds</a>
+        <a className="btn btn-outline-light" href="#ambulance">Ambulances</a>
+        <a className="btn btn-outline-light" href="#food">Food Source</a>
     </div>
-  );
+    <section id="medicine" className="comodity">
+        <h1> Medicines Availability.</h1>
+        <Location />
+    </section>
+    <section id="plasma" className="comodity">
+        <h1> Plasma Availability.</h1>
+        <Location />
+    </section>
+    <section id="oxygen" className="comodity">
+        <h1> Oxygen Availability.</h1>
+        <Location />
+    </section>
+    <section id="beds" className="comodity">
+        <h1> Beds Availability.</h1>
+        <Location />
+    </section>
+    <section id="ambulance" className="comodity">
+        <h1> Ambulances Availability.</h1>
+        <Location />
+    </section>
+    <section id="food" className="comodity">
+        <h1> Food Availability.</h1>
+        <Location />
+    </section>
+    </div>
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/Location.js
@@ -0,0 +1,105 @@
+import React from "react"
+
+function Location(){
+    return (
+      <div>
+        <div className="row">
+            <div className="col-sm">
+                Uttar Pradesh<br></br>
+                <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Select City.
+                    </button>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a className="dropdown-item" href="#">Lorem</a></li>
+                        <li><a className="dropdown-item" href="#">lorem</a></li>
+                        <li><a className="dropdown-item" href="#">lorem</a></li>
+                    </ul><br></br>
+                </div>
+            </div>
+
+            <div className="col-sm">
+                Delhi
+                <br></br>
+                <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Select City.
+                    </button>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a className="dropdown-item" href="#">Lorem</a></li>
+                        <li><a className="dropdown-item" href="#">lorem</a></li>
+                        <li><a className="dropdown-item" href="#">lorem</a></li>
+                    </ul><br></br>
+                </div>
+            </div>
+            <div className="col-sm">
+                Maharashtra
+                <br></br>
+                <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Select City.
+                    </button>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a className="dropdown-item" href="#">Lorem</a></li>
+                        <li><a className="dropdown-item" href="#">lorem</a></li>
+                        <li><a className="dropdown-item" href="#">lorem</a></li>
+                    </ul><br></br>
+                </div>
+            </div>
+        </div>
+      
+        <div className="row">
+            <div className="col-sm">
+                Karnataka
+                <br></br>
+                <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Select City.
+                    </button>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a className="dropdown-item" href="#">Lorem</a></li>
+                        <li><a className="dropdown-item" href="#">lorem</a></li>
+                        <li><a className="dropdown-item" href="#">lorem</a></li>
+                    </ul><br></br>
+                </div>
+            </div>
+            <div className="col-sm">
+                Gujarat
+                <br></br>
+                <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Select City.
+                    </button>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a className="dropdown-item" href="#">Lorem</a></li>
+                        <li><a className="dropdown-item" href="#">lorem</a></li>
+                        <li><a className="dropdown-item" href="#">lorem</a></li>
+                    </ul><br></br>
+                </div>
+            </div>
+            <div className="col-sm">
+                Kerela
+                <br></br>
+                <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Select City.
+                    </button>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a className="dropdown-item" href="#">Lorem</a></li>
+                        <li><a className="dropdown-item" href="#">lorem</a></li>
+                        <li><a className="dropdown-item" href="#">lorem</a></li>
+                    </ul><br></br>
+                </div>
+            </div>
+        </div>
+      </div>
+    )
+  }
+
+  export default Location
